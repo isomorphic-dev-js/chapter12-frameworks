8d79bd14693e493899c5d77cc79b60bc8183a4a4
elyseko <elyse1213+github@gmail.com>
2017-07-08 22:45:31 -0700

    chore: remove code that will be added by reader

--- ember-universal/app/routes/post-detail.js
+++ ember-universal/app/routes/post-detail.js
@@ -1,18 +1,4 @@
 import Ember from 'ember';
-import fetch from 'ember-fetch/ajax';
-
 
 export default Ember.Route.extend({
-  model(params) {
-    return fetch(`http://localhost:3535/post/${params.id}`)
-      .then(function(response) {
-        return fetch(`http://localhost:3535/post/${response.id}/comments`)
-          .then(function(comments) {
-            return {
-              post: response,
-              comments: comments
-            };
-          });
-      });
-  }
 });
