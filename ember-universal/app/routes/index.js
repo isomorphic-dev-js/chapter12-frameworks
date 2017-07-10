40f4737cbb6373a32ab281df9b88d4b32ebafa06
elyseko <elyse1213+github@gmail.com>
2017-07-09 20:11:56 -0700

    chore: checkin code that makes ember universal work

--- ember-universal/app/routes/index.js
+++ ember-universal/app/routes/index.js
@@ -1,4 +1,12 @@
   import Ember from 'ember';
+  import fetch from 'ember-fetch/ajax';
 
   export default Ember.Route.extend({
+    model() {
+      return fetch('http://localhost:3535/posts')
+        .then(function(response) {
+          return response;
+        });
+    }
+
   });
