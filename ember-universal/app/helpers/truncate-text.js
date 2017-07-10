b0955a2bf69a8fab284909e18b4e8ce66c6621f8
elyseko <elyse1213+github@gmail.com>
2017-07-09 20:11:01 -0700

    fix: truncate text works when body is empty

--- ember-universal/app/helpers/truncate-text.js
+++ ember-universal/app/helpers/truncate-text.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export function truncateText(opts) {
   const body = opts[0];
   const limit = opts[1];
-  return body.slice(0, limit ? limit : body.length);
+  if (body) {
+    return body.slice(0, limit ? limit : body.length);
+  } else {
+    return "";
+  }
 }
 
 export default Ember.Helper.helper(truncateText);
