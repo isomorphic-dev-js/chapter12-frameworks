09d3caa8158b58be73b7c2f7a5cd6a10db92c657
elyseko <elyse1213+github@gmail.com>
2017-07-13 00:02:12 -0700

    chore: cleanup build files

--- angular2/src/app/comment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Comment = (function () {
-    function Comment() {
-    }
-    return Comment;
-}());
-exports.Comment = Comment;
-exports["default"] = Comment;
